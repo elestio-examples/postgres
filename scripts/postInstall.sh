ed58c8e0b8ea3923f470e6ac76355284be16fa90
David <30422759+davalimi@users.noreply.github.com>
2024-03-21 16:46:09 +0200

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,8 @@
 #nothing to do
+set -o allexport; source .env; set +o allexport;
+
+docker-compose down
+openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
+sleep 5s;
+sed -i 's/#command:/command:/' docker-compose.yml
+docker-compose up -d;
