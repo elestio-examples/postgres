200196161ea80d2462b5845a1ddb6c02f65b29c3
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-15 14:20:29 +0300

    update postRestore.sh

--- scripts/postRestore.sh
+++ scripts/postRestore.sh
@@ -1,3 +1,5 @@
+set -o allexport; source .env; set +o allexport;
+
 #Restart the stack after files are restored
 docker-compose up -d;
 sleep 5s;
@@ -6,4 +8,4 @@ sleep 5s;
 gunzip < ./lastDump.sql.gz | docker-compose exec -T postgres psql -U postgres
 
 ##set the postgres password (usefull when migrating from another server)
-docker exec -i postgres psql -U postgres -c "ALTER ROLE postgres WITH PASSWORD '$SOFTWARE_PASSWORD';"
+docker-compose exec -T postgres psql -U postgres -c "ALTER ROLE postgres WITH PASSWORD '$SOFTWARE_PASSWORD';"
