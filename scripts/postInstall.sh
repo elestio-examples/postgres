f2ef0d88311075701983b82d2fd049f321e339e5
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-06-04 19:44:46 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,6 +1,7 @@
 set -o allexport; source .env; set +o allexport;
 
 docker-compose down
+sleep 15s;
 openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
 sleep 5s;
 sed -i 's/#command:/command:/' docker-compose.yml
